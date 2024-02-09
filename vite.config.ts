9016c738ea8970ad7445b92eed3d18ced56c7eae
KLisabeth <katie_lisabeth@yahoo.com>
2024-02-09 20:40:28 +0100

    other adjustments

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,8 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.NODE_ENV === 'production'
-  ? 'https://klisabeth.github.io/'
-  : '/',
+  ? '/'
+  : 'https://klisabeth.github.io/',
   plugins: [
     vue(),
     vueJsx(),
