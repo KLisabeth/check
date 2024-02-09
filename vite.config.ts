9935c739849dd36849124c312e3afc1ec7536b2c
KLisabeth <katie_lisabeth@yahoo.com>
2024-02-09 20:54:42 +0100

    extra guide

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'https://klisabeth.github.io/',
+  base:'/check/',
   plugins: [
     vue(),
     vueJsx(),
