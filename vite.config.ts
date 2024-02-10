06c5fa26021205ab042e8b64daabdb3519d7d00c
KLisabeth <katie_lisabeth@yahoo.com>
2024-02-10 23:43:57 +0100

    Update build configuration for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production'
-  ? '/check/'
-  : '/',
+  base: '/check/', // Update base URL according to your GitHub Pages URL
   plugins: [
     vue(),
     vueJsx(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': path.resolve(__dirname, 'src') // Adjust the alias resolution to use path.resolve instead of URL
     }
   }
 })
